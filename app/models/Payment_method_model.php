98d9ed57d75bbc5a1fa0078ec25dfa4b94b5215d
samidss56 <adimas.id@gmail.com>
2024-03-16 03:27:20 +0700

    feat: method payment CRUD

---
+++ app/models/Payment_method_model.php
@@ -0,0 +1,68 @@
+<?php
+
+class Payment_method_model
+{
+    private $table = "master_tipe_pembayaran";
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getAllTransaksi()
+    {
+        $this->db->query("SELECT * FROM $this->table");
+        return $this->db->resultAll();
+    }
+
+    public function addTransaksi($data)
+    {
+        $query = "INSERT INTO $this->table (tipe_pembayaran, kategori_tipe_pembayaran, tipe_pembayaran_id) 
+        VALUES (:tipe_pembayaran, :kategori_tipe_pembayaran, :tipe_pembayaran_id)";
+        try {
+            $this->db->query($query);
+            $this->db->bind(':tipe_pembayaran_id', $data['tipe_pembayaran_id']);
+            $this->db->bind(':tipe_pembayaran', $data['tipe_pembayaran']);
+            $this->db->bind(':kategori_tipe_pembayaran', $data['kategori_tipe_pembayaran']);
+            $this->db->execute();
+            return $this->db->rowCount();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return -1;
+        }
+    }
+
+    public function editTransaksi($data)
+    {
+        $query = "UPDATE $this->table SET 
+            tipe_pembayaran = :tipe_pembayaran,
+            kategori_tipe_pembayaran = :kategori_tipe_pembayaran
+            WHERE tipe_pembayaran_id = :tipe_pembayaran_id";
+        try {
+            $this->db->query($query);
+            $this->db->bind(':tipe_pembayaran_id', $data['tipe_pembayaran_id']);
+            $this->db->bind(':tipe_pembayaran', $data['tipe_pembayaran']);
+            $this->db->bind(':kategori_tipe_pembayaran', $data['kategori_tipe_pembayaran']);
+            $this->db->execute();
+            return $this->db->rowCount();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return -1;
+        }
+    }
+
+    public function deleteTransaksi($id)
+    {
+        $query = "DELETE FROM $this->table WHERE tipe_pembayaran_id = :tipe_pembayaran_id";
+        try {
+            $this->db->query($query);
+            $this->db->bind(':tipe_pembayaran_id', $id);
+            $this->db->execute();
+            return $this->db->rowCount();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return -1;
+        }
+    }
+}
